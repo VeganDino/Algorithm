6b2711ea09e3f8933080ea4fa1e2899b487dd381
min <janfar@naver.com>
2021-08-24 05:31:35 +0900

    readme

---
+++ BOJ/Bronze/Bronze2/2161번.py
@@ -0,0 +1,35 @@
+# 카드1
+# https://www.acmicpc.net/problem/2161
+
+from collections import deque
+
+n=int(input())
+m=deque([i+1 for i in range(n)])
+card=[]
+
+while len(m) > 1:
+    card.append(m.popleft())
+    m.append(m.popleft())
+
+card.append(m[0])
+for i in card: print(i, end=' ')
+
+
+''' 긴버전
+from collections import deque
+
+n=int(input())
+m=deque([i+1 for i in range(n)])
+card=[]
+
+while len(m) > 1:
+    left = m.popleft()
+    card.append(left)
+
+    left = m.popleft()
+    m.append(left)
+
+card.append(m[0])
+for i in card:
+    print(i, end=' ')
+'''
