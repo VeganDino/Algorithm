8ad46f1b15ce8aa02cf6d4fcdc571f854c6ce202
min <janfar@naver.com>
2021-09-03 01:29:58 +0900

    Algorithm Easy

---
+++ Leetcode/1. Two Sum.py
@@ -0,0 +1,22 @@
+###### 방법 1 Runtime : 1035ms
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for idx, i in enumerate(nums):
+            if target-i in nums and idx != nums.index(target-i):
+                return sorted([idx, nums.index(target-i)])
+
+
+#-----------------------------------------------------------------------
+
+
+###### 방법 2 Runtime : 103ms
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap={}
+        for idx, i in enumerate(nums):
+            if target-i in hashmap:
+                return [hashmap[target-i], idx]
+            hashmap[i]=idx
+            
