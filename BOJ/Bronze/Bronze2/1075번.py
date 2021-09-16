f03f66265a35b9fe111be718f0e3d5584f2ecc86
min <janfar@naver.com>
2021-09-17 00:30:54 +0900

    나누기

--- BOJ/Bronze/Bronze2/1075번.py
+++ BOJ/Bronze/Bronze2/1075번.py
@@ -1,6 +1,8 @@
 # 나누기
 # https://www.acmicpc.net/problem/1075
 
+###### 방법 1  68ms
+
 n,f=input(),int(input())
 
 for i in range(100):
@@ -12,3 +14,16 @@ for i in range(100):
         if int(n[:-2]+str(i))%f==0:
             print(i)
             break
+
+
+#-------------------------------------------------
+
+###### 방법 2  76ms
+
+n,f=input(), int(input())
+a=int(n[:-2]+'00')
+while True:
+    if a%f==0:
+        break
+    a+=1
+print(str(a)[-2:])
