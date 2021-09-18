99c1ba22da9f4517be43ed92ec8787f3e58b3ccb
min <janfar@naver.com>
2021-09-18 10:20:26 +0900

    연산자 끼워넣기

---
+++ BOJ/Silver/Silver1/14888번.py
@@ -0,0 +1,43 @@
+# 연산자 끼워넣기
+# https://www.acmicpc.net/problem/14888
+
+###### 방법 1 순열 
+###### pypy3 980ms , python3 시간초과
+
+from itertools import permutations
+import sys
+
+input=sys.stdin.readline
+n=int(input())
+a=list(map(int, input().split()))
+op_num=list(map(int, input().split()))
+oper=['+','-','*','/']
+lst=[]
+mini=1e9
+maxi=-1e9
+
+for i in range(4):
+    for j in range(op_num[i]):
+        lst.append(oper[i])
+
+def calculate():
+    global mini, maxi
+    for i in permutations(lst,n-1):
+        math=a[0]
+        for j in range(1,n):
+            if i[j-1]=='+':
+                math+=a[j]
+            elif i[j-1]=='-':
+                math-=a[j]
+            elif i[j-1]=='*':
+                math*=a[j]
+            elif i[j-1]=='/':
+                math=int(math/a[j])
+        if math>maxi:
+            maxi=math
+        if math<mini:
+            mini=math
+            
+calculate()
+print(maxi)
+print(mini)
