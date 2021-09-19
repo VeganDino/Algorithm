187aa6d44226ed288d6c793b96bb6e3ca5794d97
min <janfar@naver.com>
2021-09-19 10:00:29 +0900

    로또

--- BOJ/Silver/Silver2/6603번.py
+++ BOJ/Silver/Silver2/6603번.py
@@ -33,3 +33,20 @@ for i in range(len(lst)-1):
     for j in a:
         print(j[0],j[1],j[2],j[3],j[4],j[5])
     print()
+
+#----------------------------------------------------
+
+
+###### 방법 2 76ms
+
+import sys
+from itertools import combinations
+
+while True:
+    input_list = list(map(int, sys.stdin.readline().rstrip().split(" ")))
+    if input_list[0] == 0:
+        break
+
+    for i in combinations(input_list[1:], 6):
+        print(str(i).replace("(", "").replace(")", "").replace(",", ""))
+    print()
